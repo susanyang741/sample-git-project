fdbe737d8899cf4e59c6313ddf5bcd8cc127511e
susan.yang <susan.yang@everymatrix.com>
2018-05-11 10:31:56 +0000

    added getter/setter for field 'age'

--- src/org/susanyang/User.java
+++ src/org/susanyang/User.java
@@ -5,6 +5,14 @@ public class User {
     private String name;
     private int age;
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
