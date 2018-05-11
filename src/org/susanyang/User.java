ac69f9cc4214a02c8dc70c2e73bebaf215ebebb4
susan.yang <susan.yang@everymatrix.com>
2018-05-11 09:04:14 +0000

    add getter/setter

--- src/org/susanyang/User.java
+++ src/org/susanyang/User.java
@@ -3,4 +3,12 @@ package org.susanyang;
 public class User {
 
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
